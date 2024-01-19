fa46a7526552db117296337e30809d2b7436acfc
BaslealAkliluNeymar <baslealaklilu@gmail.com>
2024-01-19 20:43:59 +0300

    Done Some

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+    if (a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b))
+    {
+        return 'ERROR'
+    } 
+    else if(a > b){
+        return sumAll(b,a)
+    }
+    else{
+        sum = 0
+        for(i = a ; i <= b ;i++){
+            sum += i
+        }
+        return sum
+    }
 
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+console.log(sumAll(124,1))
